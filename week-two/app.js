0d3b88979d4afcfafb552c411a0b5df0ed49acca
0xZaddyy <oyinoladapo5@gmail.com>
2025-05-11 09:25:17 +0100

    feat: implemented all CRUD operations

--- week-two/app.js
+++ week-two/app.js
@@ -4,6 +4,48 @@ const app = express();
 
 app.use(bodyParse.json());
 
+
+let items =[
+    {id: 1, name: 'item 1' },
+    {id: 2, name: 'item 2' },
+];
+
+// Create
+app.post('/items', (req, res) => {
+    const newItem = req.body;
+    items.push(newItem);
+    res.status(201).json(newItem);
+
+
+});
+
+// Read
+app.get('/items', (req, res) => {
+    res.json(items);
+})
+
+// Update
+
+app.put('/items/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const  UpdatedItem =req.body;
+
+    items = items.map(item =>  (item.id === itemId ? UpdatedItem : item) )
+
+    res.json(UpdatedItem);
+});
+
+// Delete
+
+app.delete('items/:id', (req, res) => {
+   const itemId = parseInt(req.params.id);
+   items = items.filter(item => item.id !== itemId );
+
+   res.sendStatus(204);
+});
+
+
+
 const PORT = 8080;
 
 
